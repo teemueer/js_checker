fea0ecfb33a079478b2f328614dc3f890158893c
Teemu Eerola <teemueer@metropolia.fi>
2023-03-22 18:20:07 +0200

    jest supertest

---
+++ app.test.js
@@ -0,0 +1,36 @@
+const app = require("./app");
+const supertest = require("supertest");
+
+const api = supertest(app);
+
+describe("test with working solution", () => {
+  it("m1-02", async () => {
+    const data = {
+      url: "http://users.metropolia.fi/~teemueer/assignments/module1/02/02.html",
+      exercise: "m1-02",
+    };
+
+    const res = await api.post("/test").send(data);
+    expect(res.body.find((r) => r.result === "FAIL")).toBe(undefined);
+  });
+
+  it("m1-03", async () => {
+    const data = {
+      url: "http://users.metropolia.fi/~teemueer/assignments/module1/03/03.html",
+      exercise: "m1-03",
+    };
+
+    const res = await api.post("/test").send(data);
+    expect(res.body.find((r) => r.result === "FAIL")).toBe(undefined);
+  });
+
+  it("m3-11", async () => {
+    const data = {
+      url: "http://users.metropolia.fi/~teemueer/assignments/module3/t11/11.html",
+      exercise: "m3-11",
+    };
+
+    const res = await api.post("/test").send(data);
+    expect(res.body.find((r) => r.result === "FAIL")).toBe(undefined);
+  });
+});
--- index.js
+++ index.js
@@ -1,110 +1,15 @@
-import puppeteer from "puppeteer";
-import fs from "fs";
-import crypto from "crypto";
+const app = require("./app");
+const port = 3000;
 
-class Parser {
-  constructor(page, json) {
-    this.page = page;
-    this.json = json;
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-    this.promptCount = 0;
-    this.page.on("dialog", async (dialog) => {
-      const promptValue = this.json.prompts[this.promptCount++];
-      await dialog.accept(promptValue);
-    });
-  }
-  async getElements(css) {
-    // wait for the element to be visible
-    await this.page.waitForSelector(css);
-    // return list of elements matching the css selector
-    return await this.page.$$(css);
-  }
-
-  async getElementText(element) {
-    return await this.page.evaluate((element) => element.textContent, element);
-  }
-
-  async getElementAttribute(element, attr) {
-    return await this.page.evaluate(
-      (element, attr) => element.getAttribute(attr),
-      element,
-      attr
-    );
-  }
-
-  async clickElement(element) {
-    await this.page.evaluate((element) => element.click(), element);
-  }
-
-  async parse(objects = this.json.elements, path = []) {
-    for (const obj of objects) {
-      // wait for a while for debugging
-      await this.page.waitForTimeout(500);
-      // keep track of nested elements
-      const currentPath = [...path, obj];
-      // css selector path
-      const css = currentPath.map((obj) => obj.name).join(" ");
-      // elements on page matching the css selector
-      const elements = await this.getElements(css);
-
-      if (elements.length) {
-        console.log("PASS", css);
-      } else {
-        console.log("FAIL", css);
-      }
-
-      if (obj.attrs) {
-        for (const [key, value] of Object.entries(obj.attrs)) {
-          const attr = await this.getElementAttribute(elements[0], key);
-          if ((typeof value === "boolean" && attr) || attr == value) {
-            console.log("PASS", css, `: ${key} == ${attr}`);
-          } else {
-            console.log("FAIL", css, `: ${key} == ${attr} instead of ${value}`);
-          }
-        }
-      }
-
-      if (obj.innerHTML) {
-        const innerHTML = (await this.getElementText(elements[0])).toString();
-
-        if (innerHTML.includes(obj.innerHTML)) {
-          console.log("PASS", css, innerHTML);
-        } else {
-          console.log("FAIL", css, `${obj.innerHTML} not found in element`);
-        }
-      }
-
-      if (obj.action) {
-        if (obj.action == "click") {
-          console.log("*** DEBUG", "click!");
-          await this.clickElement(elements[0]);
-        }
-      }
-
-      if (obj.children) {
-        await this.parse(obj.children, currentPath);
-      }
-    }
-  }
-}
-
-const keypress = async () => {
-  console.log("print any key to continue");
-  process.stdin.setRawMode(true);
-  return new Promise((resolve) =>
-    process.stdin.once("data", () => {
-      process.stdin.setRawMode(false);
-      resolve();
-    })
-  );
-};
-
-const test = async (url, jsonFilepath) => {
+/*
+const main = async (url, jsonFilepath) => {
   const data = fs.readFileSync(jsonFilepath, "utf-8");
   const json = await JSON.parse(data);
 
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
 
   const page = await browser.newPage();
@@ -117,10 +22,13 @@ const test = async (url, jsonFilepath) => {
 
   await keypress();
   await browser.close();
+
+  return parser.results;
 };
 
 const url =
   "http://users.metropolia.fi/~teemueer/assignments/module1/03/03.html";
 const jsonFilepath = "./json/module1/03.json";
 
-test(url, jsonFilepath).then(process.exit);
+main(url, jsonFilepath).then(process.exit);
+*/
