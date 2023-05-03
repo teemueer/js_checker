c1042b044c3a08ba9929cd3e558ec6be72cd1c35
Teemu Eerola <tteerola@gmail.com>
2023-05-03 11:09:05 +0300

    userextractor for /api/student/courses/:id

--- controllers/students.js
+++ controllers/students.js
@@ -2,19 +2,29 @@ const express = require("express");
 const Student = require("../models/student");
 const Assignment = require("../models/assignment");
 const Course = require("../models/course");
+const { userExtractor } = require("../utils/middleware");
 const router = express.Router();
 
 //Get all students
+/*
 router.get("/", async (req, res) => {
   const assignments = await Student.find().sort({ username: 1 });
   res.json(assignments);
 });
+*/
 
 //Get students that are enrolled to a certain course.
-router.post("/test", async (req, res) => {
-  const { id } = req.body;
-  const response = await Student.find({ courses: { _id: id } });
-  return res.json(response);
+router.get("/course/:id", userExtractor, async (req, res) => {
+  const courseId = req.params.id;
+
+  if (!req.user.courses.includes(courseId)) {
+    return res
+      .status(403)
+      .json({ error: "You can only get students from your own courses" });
+  }
+
+  const students = await Student.find({ courses: { _id: courseId } });
+  return res.json(students);
 });
 
 router.get("/assignment/:id", async (req, res) => {
